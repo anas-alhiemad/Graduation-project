a2f1ba2d5fd7e693d39fd7486f7ecbf2990f43ba
nourhan <nurhannsulima@gmail.com>
2025-06-06 17:15:57 +0200

    edit

--- app/Services/CourseSectionServices/DisplayCourseSectionService.php
+++ app/Services/CourseSectionServices/DisplayCourseSectionService.php
@@ -14,30 +14,23 @@ class DisplayCourseSectionService
 
 
 
-   public function indexSection($courseID)
-{
-    // جلب السكاشن مع العلاقات weekDays و trainers
-    $sections = $this->courseSectionRepository->getAllByCourseId($courseID);
-    $sections->load(['trainers', 'weekDays']); // تحميل العلاقات
+    public function indexSection($courseID)
+    {
+      return  $sections = $this->courseSectionRepository->getAllByCourseId($courseID);
 
-    // تنسيق البيانات
-    $formatted = $sections->map(function ($section) {
-        return $section->only([
-            'id', 'name', 'seatsOfNumber', 'startDate', 'reservedSeats','endDate','state','courseId', 'created_at', 'updated_at'
-        ]) + [
-            'week_days' => $section->formatted_week_days,
-            'trainers' => $section->trainers->map(function ($trainer) {
-                return $trainer->only(['id', 'name', 'email', 'specialization', 'experience', 'photo']);
-            }),
-        ];
-    });
+        $formatted = $sections->map(function ($section) {
+            return $section->only([
+                'id', 'name', 'seatsOfNumber', 'startDate', 'reservedSeats','endDate','state','courseId', 'created_at', 'updated_at'
+            ]) + [
+                'week_days' => $section->formatted_week_days,
+            ];
+        });
 
-    return response()->json([
-        "message" => "All sections in the course.",
-        "sections" => $formatted,
-    ]);
-}
-
+        return response()->json([
+            "message" => "All sections in the course.",
+            "sections" => $formatted,
+        ]);
+    }
     public function indexSectionPending($courseID)
     {
       return  $sections = $this->courseSectionRepository->getAllByCourseIdIspending($courseID);
